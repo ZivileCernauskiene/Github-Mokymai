87e44f7cce2218caf84b8d27fec14daceb1de6d9
ZivileCernauskiene <labadaba@gmail.com>
2021-09-03 19:19:33 +0300

    pirmas bandymas

---
+++ Programa/Programa/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Programa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] Sarasas = { "vienas", "du", "trys", "keturi","penki","sesi" };
+            for(int i =0; i < Sarasas.Length; i++)
+            {
+                Console.WriteLine(Sarasas[i]);
+            }
+            
+            
+        }
+    }
+}
